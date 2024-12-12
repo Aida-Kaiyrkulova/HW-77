0bc750e189224824f5f8e3bffb9f7c84d77196ab
Aida-Kaiyrkulova <aida-kaiyr@yandex.ru>
2024-12-12 17:40:32 +0600

    App init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import AppToolbar from './components/UI/AppToolbar/AppToolbar.tsx';
+import { CssBaseline } from '@mui/material';
+
+const App = () => {
+
+  return (
+    <>
+      <CssBaseline />
+      <header>
+        <AppToolbar />
+      </header>
+
+    </>
+  );
+};
+
+export default App
